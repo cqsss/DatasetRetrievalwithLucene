9be319c1be0afe8e335f6d1fce2265ffb2f8c8f6
cqs <870663413@qq.com>
2020-12-07 16:46:55 +0800

    0.3.2 Update the logger.

---
+++ src/test/java/com/datasetretrievalwithlucene/demo/LoggerTest.java
@@ -0,0 +1,14 @@
+package com.datasetretrievalwithlucene.demo;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoggerTest {
+    private static Logger logger = Logger.getLogger(LoggerTest.class);
+    @Test
+    public void queryDataBaseTest(){
+        logger.info("Logger Test");
+    }
+}
