e5537563af094ef6dcb88f1680cce4c8aa4a5fca
cqs <870663413@qq.com>
2020-12-23 18:53:33 +0800

    0.4.0 Add index frequency processor

--- src/test/java/com/datasetretrievalwithlucene/demo/TermFreqTest.java
+++ src/test/java/com/datasetretrievalwithlucene/demo/TermFreqTest.java
@@ -15,25 +15,42 @@ import java.util.*;
 public class TermFreqTest {
 
     private static final Logger logger= LoggerFactory.getLogger(LoggerTest.class);
+
+    private Map<String, Integer> termMap = new HashMap<>();
+    public void readTerms(String fileName) {
+        List<Pair<String, Integer>> arr = new ArrayList<>();
+        try {
+            String str;
+            String[] tmps;
+            BufferedReader in = new BufferedReader(new FileReader(fileName));
+            while ((str = in.readLine()) != null) {
+                tmps = str.split(" ");
+                Integer tmp = 0;
+                if (termMap.containsKey(tmps[0])) {
+                    tmp = termMap.get(tmps[0]);
+                }
+                termMap.put(tmps[0], tmp + Integer.parseInt(tmps[1]));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     @Test
     public void testTermFreq() {
         try {
-            BufferedReader in = new BufferedReader(new FileReader("indexProcessor/terms_title.in"));
-            String str;
-            String[] tmp;
-            List<Pair<String, Integer>> arr = new ArrayList<>();
-            while ((str = in.readLine()) != null) {
-                tmp = str.split(" ");
-                arr.add(new Pair<>(tmp[0], Integer.parseInt(tmp[1])));
-            }
-            Collections.sort(arr, new Comparator<Pair<String, Integer>>() {
+            readTerms("indexProcessor/terms_content.in");
+            readTerms("indexProcessor/terms_notes.in");
+            readTerms("indexProcessor/terms_title.in");
+            List<Map.Entry<String, Integer>> arr = new ArrayList<>(termMap.entrySet());
+            Collections.sort(arr, new Comparator<Map.Entry<String, Integer>>() {
                 @Override
-                public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
                     return o2.getValue().compareTo(o1.getValue());
                 }
             });
             Integer cnt = 0;
-            for (Pair<String, Integer> i : arr) {
+
+            for (Map.Entry<String, Integer> i : arr) {
                 System.out.println(i.getKey() + "\t" + i.getValue());
                 cnt ++;
                 if (cnt == 100) break;
